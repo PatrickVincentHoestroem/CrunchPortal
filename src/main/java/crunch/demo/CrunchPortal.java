c39de88f93e5a396df4313a935067d8ad980f554
PatrickVincentHoestroem <patrick.vincent@live.dk>
2021-05-31 16:23:17 +0200

    Test for webhook

--- src/main/java/crunch/demo/CrunchPortal.java
+++ src/main/java/crunch/demo/CrunchPortal.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CrunchPortal {
 
     public static void main(String[] args) {
+        // HAHA
         SpringApplication.run(CrunchPortal.class, args);
     }
 
